ba65637bd4efe9ec0cb4707c8116339a1f49973d
Lara Henderson <39603232+lahdesign@users.noreply.github.com>
2021-03-16 09:12:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// let x = document.getElementById("myAudio"); 
+
+// function playAudio() { 
+//   x.play(); 
+// } 
+
+// function pauseAudio() { 
+//   x.pause();
+
+
+const onNameClick = function () {
+  //  console.log('got into function')
+  //   $('#message').text('Welcome to the moon.')
+  //   $('#message').css('background-Color', 'blue')
+  //   setTimeout(() => $('#message').text(''), 3000)
+  //   $('.container').addClass('animated rollOut');
+  //   $('#welcomePage').addClass('animated rollIn')
+  }
+
+const addHandlers = () => {
+    // $('#nextView').on('click', onMoonClick)
+  }
+
+  $(() => {
+    addHandlers()
+  })
